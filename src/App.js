42b8e77eec2377e5d1d21e7128f22321f9a4387f
mudrakpatel <mudrakpatel96@gmail.com>
2019-08-04 21:17:46 -0400

    useContext Hook - Part 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import ComponentC from './components/ComponentC';
+
+export const UserContext = React.createContext();
+export const ChannelContext = React.createContext();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <UserContext.Provider value={["Selina", "Bruce"]}>
+      <ChannelContext.Provider value={["Lois", "Clark"]}>
+        <ComponentC/>
+      </ChannelContext.Provider>
+    </UserContext.Provider>
     </div>
   );
 }
