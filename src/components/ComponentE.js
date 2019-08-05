bea98eee219b371868c54f76f8de25f2ae46f07b
mudrakpatel <mudrakpatel96@gmail.com>
2019-08-05 14:25:51 -0400

    useContext Hook - Part 3

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ComponentF from './ComponentF';
+import React ,{useContext} from 'react';
+//import ComponentF from './ComponentF';
+import { UserContext, ChannelContext } from '../App';
 
 function ComponentE(){
+
+  const user = useContext(UserContext);
+  const channel = useContext(ChannelContext);
+
   return(
     <div>
-      <ComponentF/>
+      <div>user value via useContext: {user[0]} and {user[1]}</div>
+      <div>channel value via useContext: {channel[0]} and {channel[1]}</div>
+      {/*<ComponentF/>*/}
     </div>
   )
 }
